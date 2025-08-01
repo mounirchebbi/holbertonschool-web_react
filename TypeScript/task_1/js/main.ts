691450d5a4563c5cd67a2afe9712be3c82135343
mounirchebbi <2157@holbertonschool.com>
2025-08-01 09:16:45 +0100

    Add StudentClass with interfaces and methods workOnHomework and displayName

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -37,4 +37,37 @@ const director1: Directors = {
 
 console.log(teacher3);
 console.log(director1);
-console.log(printTeacher("John", "Doe")); 
+console.log(printTeacher("John", "Doe"));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("John", "Doe");
+console.log(student.workOnHomework());
+console.log(student.displayName()); 
